1ab62e250baf33ae0525e9dd224fc76a9f6f650d
degrecci <henrique.degrecci@upx.com>
2020-07-15 18:05:18 -0300

    apollo client config

--- app/pages/_app.js
+++ app/pages/_app.js
@@ -1,5 +1,15 @@
 import 'semantic-ui-css/semantic.min.css';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/',
+  cache: new InMemoryCache(),
+});
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
 }
