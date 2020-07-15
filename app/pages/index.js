1bb5d7c2686a7712c1c842e373dcc2560c6b170c
degrecci <henrique.degrecci@upx.com>
2020-07-14 22:00:51 -0300

    Add semantic ui and login form

--- app/pages/index.js
+++ app/pages/index.js
@@ -1,33 +1,58 @@
-import Head from 'next/head'
+import Head from 'next/head';
+import {
+  Segment,
+  Header,
+  Form,
+  Button,
+  Grid,
+  Message,
+} from 'semantic-ui-react';
 
 export default function Home() {
   return (
-    <div className="container">
+    <div>
       <Head>
         <title>Login</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
       <main>
+        <Grid
+          textAlign="center"
+          style={{ height: '100vh' }}
+          verticalAlign="middle"
+        >
+          <Grid.Column style={{ maxWidth: 450 }}>
+            <Header as="h2" color="red" textAlign="center">
+              Log-in to your account
+            </Header>
+            <Form size="big">
+              <Segment>
+                <Form.Input
+                  fluid
+                  icon="user"
+                  iconPosition="left"
+                  placeholder="Username"
+                />
+                <Form.Input
+                  fluid
+                  icon="lock"
+                  iconPosition="left"
+                  placeholder="Password"
+                  type="password"
+                />
+
+                <Button color="red" fluid size="large">
+                  Login
+                </Button>
+              </Segment>
+            </Form>
+            <Message>
+              Do not have account? <a href="#">Sign Up</a>
+            </Message>
+          </Grid.Column>
+        </Grid>
       </main>
-
-      <footer>
-      </footer>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
+      <footer></footer>
     </div>
-  )
+  );
 }
