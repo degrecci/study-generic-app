4310afa42b31230cf3fffa07b143b4929f5cbebe
degrecci <henrique.degrecci@upx.com>
2020-07-14 21:01:42 -0300

    create next app

---
+++ app/pages/index.js
@@ -0,0 +1,33 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Login</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+      </main>
+
+      <footer>
+      </footer>
+
+      <style jsx global>{`
+        html,
+        body {
+          padding: 0;
+          margin: 0;
+          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+            sans-serif;
+        }
+
+        * {
+          box-sizing: border-box;
+        }
+      `}</style>
+    </div>
+  )
+}
