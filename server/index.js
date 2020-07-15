c3cd40085660a47539344d3e74a273306c5c5fe3
degrecci <henrique.degrecci@upx.com>
2020-07-14 22:41:18 -0300

    start session with express session

--- server/index.js
+++ server/index.js
@@ -1,16 +1,40 @@
 const { GraphQLServer } = require('graphql-yoga');
+const session = require('express-session');
+const ms = require('ms');
 
 const typeDefs = `
   type Query {
-    hello(name: String): String!
+    isLogin: Boolean!
   }
-`
+
+  type Mutation {
+    login(username: String!, password: String!): Boolean!
+    signup(username: String!, password: String!): Boolean!
+  }
+`;
 
 const resolvers = {
   Query: {
-    hello: (_, { name}) => `Hello ${name || 'World'}`,
+    isLogin: (parent, args, { request }) =>
+      typeof request.session.user !== 'undefined',
   },
-}
+};
 
-const server = new GraphQLServer({ typeDefs, resolvers });
-server.start(() => console.log('Server is running on localhost:4000'))
+const context = ({ request }) => ({ request });
+
+const server = new GraphQLServer({ typeDefs, resolvers, context });
+
+server.express.use(
+  session({
+    name: 'qid',
+    secret: `some-random-secret-here`,
+    resave: true,
+    saveUninitialized: true,
+    cookie: {
+      secure: process.env.NODE_ENV === 'production',
+      maxAge: ms('1d'),
+    },
+  })
+);
+
+server.start(() => console.log('Server is running on localhost:4000'));
